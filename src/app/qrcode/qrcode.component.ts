66c58217cd8f38e3da5a0defe1b05c1d26e080ad
Nowicki <Thomas.Nowicki@viadee.de>
2019-05-28 12:11:31 +0200

    Using different Tool

--- src/app/qrcode/qrcode.component.ts
+++ src/app/qrcode/qrcode.component.ts
@@ -1,76 +1,40 @@
-import { Component, VERSION, OnInit, ViewChild } from '@angular/core';
-
-import { ZXingScannerComponent } from '@zxing/ngx-scanner';
-
-import { Result } from '@zxing/library';
-
+import {Component, ViewChild, ViewEncapsulation, OnInit} from '@angular/core';
+import {QrScannerComponent} from 'angular2-qrscanner';
 @Component({
   selector: 'app-qrcode',
   templateUrl: './qrcode.component.html',
   styleUrls: ['./qrcode.component.scss']
 })
 export class QrcodeComponent implements OnInit {
+  @ViewChild(QrScannerComponent) qrScannerComponent: QrScannerComponent ;
+ 
+  ngOnInit() {
+      this.qrScannerComponent.getMediaDevices().then(devices => {
+          console.log(devices);
+          const videoDevices: MediaDeviceInfo[] = [];
+          for (const device of devices) {
+              if (device.kind.toString() === 'videoinput') {
+                  videoDevices.push(device);
+              }
+          }
+          if (videoDevices.length > 0){
+              let choosenDev;
+              for (const dev of videoDevices){
+                  if (dev.label.includes('front')){
+                      choosenDev = dev;
+                      break;
+                  }
+              }
+              if (choosenDev) {
+                  this.qrScannerComponent.chooseCamera.next(choosenDev);
+              } else {
+                  this.qrScannerComponent.chooseCamera.next(videoDevices[0]);
+              }
+          }
+      });
 
-  ngVersion = VERSION.full;
-
-  @ViewChild('scanner')
-  scanner: ZXingScannerComponent;
-
-  hasDevices: boolean;
-  hasPermission: boolean;
-  qrResultString: string;
-  qrResult: Result;
-
-  availableDevices: MediaDeviceInfo[];
-  currentDevice: MediaDeviceInfo;
-
-  ngOnInit(): void {
-      this.scanner.camerasFound.subscribe((devices: MediaDeviceInfo[]) => {
-        this.hasDevices = true;
-        this.availableDevices = devices;
-
-
-      // selects the devices's back camera by default
-      // for (const device of devices) {
-      //     if (/back|rear|environment/gi.test(device.label)) {
-      //         this.scanner.changeDevice(device);
-      //         this.currentDevice = device;
-      //         break;
-      //     }
-      // }
-    });
-
-    this.scanner.hasDevices.subscribe((x: boolean) => this.hasDevices = x);
-    this.scanner.scanComplete.subscribe((x: Result) => this.qrResult = x);
-    this.scanner.permissionResponse.subscribe((x: boolean) => this.hasPermission = x);
-  }
-
-  displayCameras(cameras: MediaDeviceInfo[]) {
-    this.availableDevices = cameras;
-  }
-
-  handleQrCodeResult(resultString: string) {
-    this.qrResultString = resultString;
-  }
-
-  onDeviceSelectChange(selected: string) {
-    const device = this.availableDevices.find(x => x.deviceId === selected);
-    this.currentDevice = device || null;
-  }
-
-  stateToEmoji(state: boolean): string {
-
-    const states = {
-      // not checked
-      undefined: '❔',
-      // failed to check
-      null: '⭕',
-      // success
-      true: '✔',
-      // can't touch that
-      false: '❌'
-    };
-
-    return states['' + state];
+      this.qrScannerComponent.capturedQr.subscribe(result => {
+          console.log(result);
+      });
   }
 }
